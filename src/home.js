34f4fee21b220ab25a95496e5d379349fa4ff0fd
Matthis Böhm <matthis.boehm2@web.de>
2024-08-10 18:38:25 +0200

    Add tab switching logic

--- src/home.js
+++ src/home.js
@@ -2,22 +2,29 @@
 import "./style.css";
 import Food from "./food.jpg";
 
-export default function initialPageLoad() {
+export default function homeLoad() {
   const contentContainer = document.querySelector("#content");
+
+  // Create div
+  const div = document.createElement("div");
 
   // Add the h1
   const headline = document.createElement("h1");
   headline.textContent = "My very own restaurant";
-  contentContainer.appendChild(headline);
+  div.appendChild(headline);
 
   // Add the food image
   const myFood = new Image();
   myFood.src = Food;
-  contentContainer.appendChild(myFood);
+  div.appendChild(myFood);
 
   // Add some text
   const someText = document.createElement("p");
   someText.textContent =
     "Do you see this awesome food that we make over here? Come on and give it a taste yourself.";
-  contentContainer.appendChild(someText);
+  div.appendChild(someText);
+
+  contentContainer.appendChild(div);
+
+  return contentContainer;
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-import initialPageLoad from "./initialPageLoad";
+import homeLoad from "./home";
 import menuLoad from "./menu";
 import aboutLoad from "./about";
 
-initialPageLoad();
+const contentContainer = document.querySelector("#content");
+const navButtons = document.querySelectorAll("nav button");
+
+function registerEventListeners() {
+  navButtons.forEach((btn) => {
+    btn.addEventListener("click", switchTab);
+  });
+}
+
+function switchTab(event) {
+  contentContainer.removeChild(contentContainer.firstChild);
+
+  switch (event.target.id) {
+    case "home":
+      homeLoad();
+      break;
+    case "menu":
+      menuLoad();
+      break;
+    case "about":
+      aboutLoad();
+      break;
+    default:
+      console.log("This button is not supported yet");
+      break;
+  }
+}
+
+registerEventListeners();
+homeLoad();
