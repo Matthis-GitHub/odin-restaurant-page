097ffb904774ae5130ea4eca7744c9f6a1317f18
Matthis Böhm <matthis.boehm2@web.de>
2024-08-17 21:03:08 +0200

    Fix missing id attributes, prepare gh-pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "eval-source-map",
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
